b1f0b8d76403f51e9a167ee21fc46b7529fbf231
Bania Fonseca <bfonseca@unilurio.ac.mz>
2020-12-03 06:33:54 +0100

    small updates on gym
    Former-commit-id: e7aa97706e7024674aeaee3a7f27de7800e2d9ce
    Former-commit-id: 454e24caac3350d78cafef87d416381584698880
    Former-commit-id: 7e876a2c2e4d82a6134ccb40319b74f56c95fe7d
    Former-commit-id: 5e7397eb15227b3af58a6d936d4ee4c9ed98244b

--- gym/run.sh
+++ gym/run.sh
@@ -1,2 +1,3 @@
+chmod 777 -Rf tf_od_api/workspace/train_core/
 nvidia-docker run -it -v $(pwd)/tf_od_api/workspace/train_core:/home registry.fsoc.hpi.uni-potsdam.de/coral-ai > tf_od_api/workspace/train_core/log
 

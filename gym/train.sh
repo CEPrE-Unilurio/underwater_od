83ccc2d95d34fa90e008abbc8d0f8466c0ff16df
Bania Fonseca <bfonseca@unilurio.ac.mz>
2020-12-28 18:18:30 +0100

    added sample models on "build" directory for deployment testing
    Former-commit-id: af8fbdae59e034dcd01a242e1bb8d93bc1577862
    Former-commit-id: 2f4e4f109c56881cb76244b84fa87348c52a92e6
    Former-commit-id: e1b2ab770b7552678f4753f76b74bf519b8b1f99 [formerly 3ea66f520d422377ee4ef8bc4e28ede87efb0937]
    Former-commit-id: 74e5cc6d52a284ba2bd4521b21848b8c1e8ecb38
    Former-commit-id: ded21c6004a9b0ba63c1b631088d1fa8bb050fae

---
+++ gym/train.sh
@@ -0,0 +1,3 @@
+chmod 777 -Rf tf_od_api/workspace/train_core/
+nvidia-docker run -it -v $(pwd)/tf_od_api/workspace/train_core:/home registry.fsoc.hpi.uni-potsdam.de/coral-ai > tf_od_api/workspace/train_core/train.log
+

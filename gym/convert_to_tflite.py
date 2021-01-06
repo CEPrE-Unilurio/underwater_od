6fb8866cb1934b8e9f07d01992e9f09adf118f51
Bania Fonseca <bfonseca@unilurio.ac.mz>
2021-01-07 00:53:05 +0100

    pipeline refatored
    Former-commit-id: 3c976e46b426f50731b7250213e585d234d19de8
    Former-commit-id: b2cebf67487fbde874adadba4c666120aba0034e
    Former-commit-id: d5593d971516a25a614a0a68206a1d836b9de3b8 [formerly 7e9ccd533643fc0452f006574aa9fec4788c6d0b]
    Former-commit-id: 5c8fcc52774e73177f64aa9126fbfe65bf1f9e81
    Former-commit-id: 51d87010cd0637cc013669f08bfc0e7130b44939

--- gym/convert_to_tflite.py
+++ gym/convert_to_tflite.py
@@ -3,32 +3,28 @@ import sys, getopt
 
 def main(argv):
     saved_model_di = ''
-    tflite_model_dir = ''
+    output_file = ''
     try:
-        opts, args = getopt.getopt(argv,"hi:o:",["saved_model_dir=","tflite_model_dir="])
+        opts, args = getopt.getopt(argv,"hi:o:",["saved_model_dir=","output_file="])
     except getopt.GetoptError:
-        print('convert_to_tflite.py -i <saved_model_dir> -o <tflite_model_dir>')
+        print('convert_to_tflite.py -i <saved_model_dir> -o <output_file>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ('convert_to_tflite.py -i <saved_model_dir> -o <tflite_model_dir>')
+            print ('convert_to_tflite.py -i <saved_model_dir> -o output_file>')
             sys.exit()
         elif opt in ("-i", "--saved_model_dir"):
             saved_model_dir = arg
-        elif opt in ("-o", "--tflite_model_dir"):
-            tflite_model_dir = arg
+        elif opt in ("-o", "--output_file"):
+            output_file = arg
     
+    converter = tf.compat.v2.lite.TFLiteConverter.from_saved_model(saved_model_dir)
 
-    converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
-
-    #converter.optimizations = [tf.lite.Optimize.DEFAULT]
-    #converter.experimental_new_converter = True
-    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS]
     tflite_model = converter.convert()
 
-    with open('{}/model.tflite'.format(tflite_model_dir), 'wb') as f:
+    with open(output_file, 'wb') as f:
         f.write(tflite_model)
-        print("SavedModel converted into a TensorFlow Lite model")
+        print("Done!")
 
 if __name__ == "__main__":
    main(sys.argv[1:])

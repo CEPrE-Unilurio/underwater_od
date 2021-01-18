fe0e97e2c09b5191abe7d376181a0b2b3b9e88b0
Bania Fonseca <bfonseca@unilurio.ac.mz>
2021-01-18 20:04:32 +0100

    use make download_models to download pre-trained models
    Former-commit-id: 3b66535d8009c7552b23ebb5ebbbf12239888a80
    Former-commit-id: bc19b8b1ec5b8a96d83b5922aa785852066dad40
    Former-commit-id: 54d44d5c382d438e5a980dc04b45158eab1d182b [formerly 535dec5c698dc3293caafb01606b6661f8bf6717]
    Former-commit-id: 0f5be0cedae048b1ffe5a1fd60ffaa169a819471
    Former-commit-id: df4ab2fd3e02acf12a5067761c95ad73eefc8778

---
+++ gym/download_models.bash
@@ -0,0 +1,8 @@
+wget http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8.tar.gz
+tar -xvf ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8.tar.gz
+rm ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8.tar.gz
+mv -f ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/* ai4coral/pre-trained-models/ssd_mobilenet_v2_fpnlite
+rm -r ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8
+
+
+
